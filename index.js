bfd0de034a83e78ef00e923bc605462b4c940223
pzcuong <dok2308@gmail.com>
2022-09-12 09:21:18 +0700

    Thêm giới hạn payload

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ require('dotenv').config();
 
 var app = express();
 
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(cookieParser());
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
 app.use('/public', express.static('./public'));
 
